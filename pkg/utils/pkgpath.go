bd7fca70af2e148ff8dbc01b7d5d6cf71bb1f198
Mathew Merrick <matmerr@microsoft.com>
2025-06-03 22:52:29 +0000

    Revert "simplify pkg path"
    This reverts commit 5c525d4f45044139a5905c3b5df71e574bd90126.

--- pkg/utils/pkgpath.go
+++ pkg/utils/pkgpath.go
@@ -1,14 +1,49 @@
 package utils
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 )
 
 func GetPackagePath(data interface{}) (string, error) {
 	t := reflect.TypeOf(data)
 	pkgPath := t.PkgPath()
-	if pkgPath == "" {
-		return ".", nil
+	runningPath, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("failed to get current working directory: %w", err)
 	}
-	return pkgPath, nil
+
+	// Try to find the module root by looking for go.mod
+	modRoot := runningPath
+	for {
+		if _, err := os.Stat(filepath.Join(modRoot, "go.mod")); err == nil {
+			break
+		}
+		parent := filepath.Dir(modRoot)
+		if parent == modRoot {
+			// Reached root, not found
+			modRoot = runningPath
+			break
+		}
+		modRoot = parent
+	}
+
+	// If pkgPath is empty (main), just use current dir
+	if pkgPath == "" {
+		return runningPath, nil
+	}
+
+	// Try to find the relative path from module root to the package
+	rel := pkgPath
+	if strings.HasPrefix(pkgPath, "github.com/") {
+		// Remove module path prefix if present
+		parts := strings.SplitN(pkgPath, "/", 4)
+		if len(parts) == 4 {
+			rel = parts[3]
+		}
+	}
+	return filepath.Join(modRoot, rel), nil
 }
